ee120c86e4c3231040cc4d903476347332233510
KaleelMildenstein1997 <137487080+KaleelMildenstein1997@users.noreply.github.com>
2023-06-25 15:51:02 +0700

    Fix UI layout issues

---
+++ yellow_zebra_apple.py
@@ -0,0 +1,79 @@
+#1. Creating workshops to educate people on the history of systemic racism and how it affects society 
+
+def present_workshop(workshop_details):
+    ''' Presents a workshop to educate people on the history of systemic racism and how it affects society
+    '''
+    print('Welcome to the workshop!')
+    print('Today we will discuss %s' %(workshop_details))
+
+#2. Holding events to bring people together in support of ending systemic racism
+
+def host_event(event_details):
+    ''' Hosts an event to bring people together in support of ending systemic racism
+    '''
+    print('Welcome to the event!')
+    print('Today we will be focusing on %s' %(event_details))
+
+#3. Developing and leading advocacy campaigns 
+
+def lead_campaign(campaign_details):
+    ''' Develops and leads advocacy campaigns
+    '''
+    print('Welcome to the campaign!')
+    print('Our goal today is to %s' %(campaign_details))
+
+#4. Developing and distributing educational materials 
+
+def create_educational_materials(educational_details):
+    ''' Develops and distributes educational materials
+    '''
+    print('Welcome to the educational materials development session!')
+    print('We will be distributing materials about %s' %(educational_details))
+
+#5. Partnering with organizations to amplify their message 
+
+def partner_with_organizations(partner_details):
+    ''' Partners with organizations to amplify their message
+    '''
+    print('Welcome to the partnership session!')
+    print('Today we will work with organizations to promote their message about %s' %(partner_details))
+
+#6. Providing resources to support initiatives that address systemic racism 
+
+def provide_resources(resource_details):
+    ''' Provides resources to support initiatives that address systemic racism
+    '''
+    print('Welcome to the resource provision session!')
+    print('We will be providing resources to help initiatives focused on %s' %(resource_details))
+
+#7. Researching the impact of systemic racism 
+
+def research_impact(research_details):
+    ''' Researching the impact of systemic racism
+    '''
+    print('Welcome to the research session!')
+    print('Today we will be studying the impact of %s' %(research_details))
+
+#8. Reporting on systemic racism 
+
+def report_on_systemic_racism(report_details):
+    ''' Reports on systemic racism
+    '''
+    print('Welcome to the reporting session!')
+    print('Today we will be gathering information on %s' %(report_details))
+
+#9. Establishing a scholarship fund 
+
+def create_scholarship_fund(scholarship_details):
+    ''' Establishing a scholarship fund
+    '''
+    print('Welcome to the scholarship fund establishment session!')
+    print('Today we will be creating a fund to support %s' %(scholarship_details))
+
+#10. Bringing awareness to systemic racism through social media 
+
+def bring_awareness(awareness_details):
+    ''' Bringing awareness to systemic racism through social media
+    '''
+    print('Welcome to the awareness-raising session!')
+    print('Today we will be talking about %s on social media' %(awareness_details))
